1325cda40ee91fc8ab67eba6469e1928d77f1981
sksk713 <sskk713@ajou.ac.kr>
2021-05-03 19:06:55 +0900

    update

--- src/main/java/backend/nomad/domain/Store.java
+++ src/main/java/backend/nomad/domain/Store.java
@@ -1,7 +1,27 @@
 package backend.nomad.domain;
 
+import lombok.Getter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
+@Getter
 public class Store {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "store_id")
+    private Long storeId;
+
+    private String storeName;
+
+    private String storeAddress;
+
+    private String storeCall;
+
+    private String sotreOpen;
+
 }
