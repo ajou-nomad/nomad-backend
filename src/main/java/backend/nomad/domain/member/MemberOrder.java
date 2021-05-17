522222e51018ff373f35d2c14321a1e0d2798498
sksk713 <sskk713@ajou.ac.kr>
2021-05-17 19:50:22 +0900

    연동 테스트

--- src/main/java/backend/nomad/domain/member/MemberOrder.java
+++ src/main/java/backend/nomad/domain/member/MemberOrder.java
@@ -1,27 +1,27 @@
-package backend.nomad.domain.member;
-
-import backend.nomad.domain.group.DeliveryGroup;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-@Entity
-public class MemberOrder {
-
-    @Id
-    @GeneratedValue
-    private Long memberOrderId;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-
-}
+//package backend.nomad.domain.member;
+//
+//import backend.nomad.domain.group.DeliveryGroup;
+//import lombok.AccessLevel;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Getter
+//@Entity
+//public class MemberOrder {
+//
+//    @Id
+//    @GeneratedValue
+//    private Long memberOrderId;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//
+//
+//}
