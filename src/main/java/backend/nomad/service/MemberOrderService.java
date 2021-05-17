eae4c6ab8f0473ba3f6a274bfaf19ff2377ec443
sksk713 <sskk713@ajou.ac.kr>
2021-05-17 19:51:47 +0900

    연동 테스트

--- src/main/java/backend/nomad/service/MemberOrderService.java
+++ src/main/java/backend/nomad/service/MemberOrderService.java
@@ -1,31 +1,31 @@
-package backend.nomad.service;
-
-import backend.nomad.domain.member.MemberOrder;
-import backend.nomad.domain.member.MemberOrderRepository;
-import backend.nomad.domain.member.MemberRepository;
-import backend.nomad.dto.member.MemberOrderSaveRequestDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class MemberOrderService {
-
-    private final MemberRepository memberRepository;
-    private final MemberOrderRepository memberOrderRepository;
-
-    @Transactional
-    public Long save(MemberOrderSaveRequestDto dto) {
-        return memberOrderRepository.save(dto.toEntity()).getMemberOrderId();
-    }
-
-    @Transactional
-    public List<MemberOrder> findMemberOrders() {
-        return memberOrderRepository.findAll();
-    }
-
-
-}
+//package backend.nomad.service;
+//
+//import backend.nomad.domain.member.MemberOrder;
+//import backend.nomad.domain.member.MemberOrderRepository;
+//import backend.nomad.domain.member.MemberRepository;
+//import backend.nomad.dto.member.MemberOrderSaveRequestDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class MemberOrderService {
+//
+//    private final MemberRepository memberRepository;
+//    private final MemberOrderRepository memberOrderRepository;
+//
+//    @Transactional
+//    public Long save(MemberOrderSaveRequestDto dto) {
+//        return memberOrderRepository.save(dto.toEntity()).getMemberOrderId();
+//    }
+//
+//    @Transactional
+//    public List<MemberOrder> findMemberOrders() {
+//        return memberOrderRepository.findAll();
+//    }
+//
+//
+//}
