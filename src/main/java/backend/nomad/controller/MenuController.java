6bd530e185fc75074d22f12638e00b7a52580bb1
sksk713 <sskk713@ajou.ac.kr>
2021-05-23 18:28:59 +0900

    연동 테스트

--- src/main/java/backend/nomad/controller/MenuController.java
+++ src/main/java/backend/nomad/controller/MenuController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
@@ -27,10 +29,14 @@ public class MenuController {
     private final MemberService memberService;
     private final StoreService storeService;
 
+    private final Logger logger;
+
     @PostMapping("/menu")
     public void savaStoreMenu(@RequestBody MenuRequestDto dto, @RequestHeader("Authorization") String header) throws FirebaseAuthException {
         FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(header);
         String uid = decodedToken.getUid();
+
+        logger.info(String.valueOf(dto));
 
         Member member = memberService.findByUid(uid);
         Store store = member.getStore().get(0);
