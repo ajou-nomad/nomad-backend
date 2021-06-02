5bc8288463557b35fd53fa4c4224c15567f635b8
sksk713 <sskk713@ajou.ac.kr>
2021-06-02 19:48:59 +0900

    /storeList 수정

--- src/main/java/backend/nomad/controller/StoreController.java
+++ src/main/java/backend/nomad/controller/StoreController.java
@@ -124,11 +124,20 @@ public class StoreController {
                     .map(m -> new ReviewResponseDto(m.getReviewId(), m.getNickName(), m.getContents(), m.getImgUrl(), m.getRate(), m.getLocalDateTime()))
                     .collect(Collectors.toList());
 
-            PromotionMenu promotionMenu = x.getPromotionMenu();
-            PromotionMenuDto promotionMenuDto = new PromotionMenuDto(promotionMenu.getPromotionMenuId(), promotionMenu.getPromotionMenuName(), promotionMenu.getCost(), promotionMenu.getDescription(), promotionMenu.getPromotionDescription(), promotionMenu.getImgUrl());
+            if (x.getPromotion() != null) {
+                PromotionMenu promotionMenu = x.getPromotionMenu();
+                PromotionMenuDto promotionMenuDto = new PromotionMenuDto(promotionMenu.getPromotionMenuId(), promotionMenu.getPromotionMenuName(), promotionMenu.getCost(), promotionMenu.getDescription(), promotionMenu.getPromotionDescription(), promotionMenu.getImgUrl());
 
-            StoreListDto dto = new StoreListDto(x.getStoreId(), x.getStoreName(), x.getPhoneNumber(), x.getAddress(), x.getLatitude(), x.getLongitude(), x.getOpenTime(), x.getCloseTime(), x.getDeliveryTip(), x.getLogoUrl(), menuList, reviewList, x.getRate(), x.getNotice(), x.getStoreIntro(), x.getCategory(),x.getPromotion(), promotionMenuDto);
-            dtoList.add(dto);
+                StoreListDto dto = new StoreListDto(x.getStoreId(), x.getStoreName(), x.getPhoneNumber(), x.getAddress(), x.getLatitude(), x.getLongitude(), x.getOpenTime(), x.getCloseTime(), x.getDeliveryTip(), x.getLogoUrl(), menuList, reviewList, x.getRate(), x.getNotice(), x.getStoreIntro(), x.getCategory(),x.getPromotion(), promotionMenuDto);
+                dtoList.add(dto);
+            }
+            else {
+
+                StoreListDto dto = new StoreListDto(x.getStoreId(), x.getStoreName(), x.getPhoneNumber(), x.getAddress(), x.getLatitude(), x.getLongitude(), x.getOpenTime(), x.getCloseTime(), x.getDeliveryTip(), x.getLogoUrl(), menuList, reviewList, x.getRate(), x.getNotice(), x.getStoreIntro(), x.getCategory(),x.getPromotion(), null);
+                dtoList.add(dto);
+
+            }
+
         }
         return new Result(dtoList);
     }
